2be164c85501f9b3bd3117e0d1fd9ca1bdd4b062
WhyRoX <enes.12.yumer@gmail.com>
2025-10-03 11:02:37 +0200

    fix build

--- frontend/src/pages/AddExpense.tsx
+++ frontend/src/pages/AddExpense.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useNavigate } from 'react-router';
 import ExpenseAdd from '../components/ExpenseAdd';
 import type { ExpenseInput } from '../types/Expense';
