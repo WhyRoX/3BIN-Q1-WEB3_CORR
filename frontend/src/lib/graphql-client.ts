0d4f3a19ff4f675b4b597fc8deefb064c7db7e32
WhyRoX <enes.12.yumer@gmail.com>
2025-10-17 11:11:05 +0200

    grapql front

---
+++ frontend/src/lib/graphql-client.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
+
+const API_HOST = import.meta.env.VITE_GRAPHQL_URL;
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: API_HOST }),
+  cache: new InMemoryCache(),
+});
+
+export default client;
