0389c84e162934cb968957444634815cfbfa38bc
WhyRoX <enes.12.yumer@gmail.com>
2025-10-17 10:59:42 +0200

    graphql debut

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,18 +2,31 @@ import cors from "cors";
 import express, { type Express } from "express";
 import helmet from "helmet";
 import { pino } from "pino";
-import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter";
-import userRouter from "@/api/user/userRouter";
-import errorHandler from "@/common/middleware/errorHandler";
-import rateLimiter from "@/common/middleware/rateLimiter";
-import requestLogger from "@/common/middleware/requestLogger";
-import { env } from "@/common/utils/envConfig";
-import expenseRouter from "./api/expense/expenseRouter";
-import transferRouter from "./api/transfer/transferRouter";
-import transactionRouter from "./api/transaction/transactionRouter";
+import { healthCheckRouter } from "@/api/healthCheck/healthCheckRouter.js";
+import userRouter from "@/api/user/userRouter.js";
+import errorHandler from "@/common/middleware/errorHandler.js";
+import rateLimiter from "@/common/middleware/rateLimiter.js";
+import requestLogger from "@/common/middleware/requestLogger.js";
+import { env } from "@/common/utils/envConfig.js";
+import expenseRouter from "./api/expense/expenseRouter.js";
+import transferRouter from "./api/transfer/transferRouter.js";
+import transactionRouter from "./api/transaction/transactionRouter.js";
+import graphqlMiddleware from "./graphql/middleware.js";
+import { ruruHTML } from "ruru/server";
 
 const logger = pino({ name: "server start" });
 const app: Express = express();
+
+if (env.isDevelopment) {
+  const config = { endpoint: "/graphql" };
+  // Serve Ruru HTML
+  app.get("/ruru", (req, res) => {
+    res.format({
+      html: () => res.status(200).send(ruruHTML(config)),
+      default: () => res.status(406).send("Not Acceptable"),
+    });
+  });
+}
 
 // Set the application to trust the reverse proxy
 app.set("trust proxy", true);
@@ -22,6 +35,7 @@ app.set("trust proxy", true);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use("/graphql", graphqlMiddleware);
 app.use(helmet());
 app.use(rateLimiter);
 
@@ -30,6 +44,7 @@ app.use(requestLogger);
 
 // Routes
 app.use("/health-check", healthCheckRouter);
+
 app.use("/api/users", userRouter);
 app.use("/api/expenses", expenseRouter);
 app.use("/api/transfers", transferRouter);
@@ -38,4 +53,5 @@ app.use("/api/transactions", transactionRouter);
 // Error handlers
 app.use(errorHandler());
 
+// Export app and logger so they can be imported by index.ts
 export { app, logger };
